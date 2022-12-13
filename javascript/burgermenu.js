1b91554786a06654cd7c3ee8f4528ea797ba2598
Marie Kjærside <m.kjaerside@outlook.com>
2022-12-13 23:40:48 +0100

    forside padding

--- javascript/burgermenu.js
+++ javascript/burgermenu.js
@@ -6,19 +6,6 @@ const menu = document.querySelector(".main-menu");
 // Lav funktionen "toggleMenu()" med følgende funktionalitet
 function toggleMenu() {
   menu.classList.toggle("kryds");
-  // const menuShown = menu.classList.contains("shown");
-  /*
-  // spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
-  if (menuShown) {
-    console.log(menuShown); // se i konsollen
-    // sæt btn.textContent til "Luk", hvis menuShown er "true"
-    btn.classList.add("luk");
-  } else {
-    console.log(menuShown); // se i konsollen
-    // sæt btn.textContent til "Menu", hvis menuShown er "false"
-    btn.classList.add("burger");
-  }
-  */
 }
 
 btn.addEventListener("click", toggleMenu);
